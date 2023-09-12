60eaa5f9b4e4376e1c176e3b050180ca07040697
hermannkiffou <hermann.kiffou@gmail.com>
2023-09-12 16:58:13 +0000

    Mise à jours NavBar de la page index

---
+++ javascript/component/componants.js
@@ -0,0 +1,20 @@
+
+  <div class="indexNav" id="indexNav">
+      <p class="middelBank">MiddleBank</p>
+      <nav>
+          <ul>
+              <li>Accueil</li>
+              <li>A propos</li>
+              <li>Service</li>
+              <li>Equipe</li>
+          </ul>
+      </nav>
+      <div class="infoContact">
+          <img src="" alt=""></img>
+          <div class="divText">
+              <p class="appellerNous">Appeller Nous au :</p>
+              <p class="contact">+225 01 02 00 68 69</p>
+          </div>
+      </div>
+  </div>
+
